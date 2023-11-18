729d57deafaaaea8de33359a889f74ed19d89650
nguyentrungphap <phapnguyen05032002@gmail.com>
2023-11-18 10:30:44 +0700

    a

--- src/Api/axiosInstance.js
+++ src/Api/axiosInstance.js
@@ -2,6 +2,7 @@ import AppUrl from "./AppUrl";
 import axios from "axios";
 const axiosInstanceConfig = {
   baseURL: AppUrl.BaseURL,
+  // baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     Authorization: `Bearer ${AppUrl.token}`,
     "Content-Type": "application/json",
